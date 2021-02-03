3a3182e6c489cc3fd747aaa7a58ac48acd52d2c9
sashofthedark <sasha.bakman@gmail.com>
2021-02-03 15:34:03 +0200

    created main and added the functions for other files

---
+++ main.py
@@ -0,0 +1,9 @@
+from vectors import OneQubitVector,TwoQubitVector,ThreeQubitVector,VectorConv
+from gates import OneQubitGates,TwoQubitGates,ThreeQubitGates
+
+def Main():
+    #this is the main function which performs the error correction procedure
+    pass
+
+if __name__ == '__main__':
+    Main()
