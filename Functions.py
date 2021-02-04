3181666a917d9316dd93817fbf970fb11a081e8c
sashofthedark <sasha.bakman@gmail.com>
2021-02-04 15:56:46 +0200

    Written the algorithm in main and the algorithm for the CircuitAndCorrection function in Functions

--- Functions.py
+++ Functions.py
@@ -42,3 +42,28 @@ class VectorConv():
         #construct is either a column vector, a row vector or a matrix
         #it's Const1 X Const2 X Const3 (first 2 and 3, then 1 with the resulting product)
         return np.kron(np.kron(construct2,construct3),construct1)
+
+def CircuitAndCorrection(
+    prob_x, 
+    prob_p, 
+    UpperQubit = VectorOfQubits,
+    LowerQubit = VectorOfQubits, 
+    result = VectorOfQubits):
+    #pass the first and second qubits separately to ApplyNoise function 
+    # which will apply noise to them, pass the probabilities to this function
+    #Apply Hadamard gate to upper qubit
+
+    #create an entangled state of three qubits for the upper qubit (with |00> as ancilla)
+    #create an entangled state of three qubits for the lower qubit (with |00> as ancilla)
+
+    #Apply BitFlip error correction circuit to upper three qubits
+    #Apply BitFlip error correction circuit to lower three qubits
+
+    #Apply Hadamard gate to upper qubit (create a three-qubit gate with two unity one-qubit matrices)
+
+    #Create a two-qubit state from upper and lower qubit (discard the ancilla qubits)
+    #apply CNOT gate to the two-qubit state
+
+    #compare with desired result 
+    #return True if comparison successful, and False otherwise.
+    pass
--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from gates import OneQubitGates,TwoQubitGates,ThreeQubitGates
 from Functions import VectorConv
 
 def Main():
-    #this is the main function which performs the error correction procedure
-    pass
+    #define a list of probabilities for X error gate
+    #define a list of probabilities for the Z error gate
+    #define upper and lower qubits (as |0> and |+> states)
+    #run the function which applies the noise and error correction and returns a list of Boolean results 
+    #(True if error correction succeeded and False otherwise)
+    #function parameters are initial states (upper and lower qubits), prob_X, prob_Z, and desired final state. 
 
 if __name__ == '__main__':
     Main()
