14460bd2d96c6a9972540c9d10f03f686b9d0717
sashofthedark <sasha.bakman@gmail.com>
2021-02-05 22:29:10 +0200

    added test_functions and test_vectorconv and made a skeleton of the test functions

---
+++ tests/test_functions.py
@@ -0,0 +1,20 @@
+import numpy as np
+from math import sqrt
+import unittest
+from src.functions import PerformBitFlipCorrection,ApplyNoise,RetrieveFirstQubit,CircuitAndCorrection
+
+class TestFunctions(unittest.TestCase):
+    def test_PerformBitFlipCorrection(self):
+        pass
+
+    def test_ApplyNoise(self):
+        pass
+
+    def test_RetrieveFirstQubit(self):
+        pass
+
+    def test_CircuitAndCorrection(self):
+        pass
+
+if __name__ == 'main':
+    unittest.main
---
+++ tests/test_vectorconv.py
@@ -0,0 +1,11 @@
+import numpy as np
+from math import sqrt
+import unittest
+from src.vectorconv import VectorConv
+
+class TestVectorConv(unittest.TestCase):
+    def test_VectorConv(self):
+        pass
+
+if __name__ == 'main':
+    unittest.main
